cbdd183787984456f600f2445b6589baf52f0c03
William <p23wclegg@fairfieldprep.org>
2022-12-07 12:24:32 -0500

    asdfasdfasdf

---
+++ Lab_8-1.py
@@ -0,0 +1,8 @@
+#Lab_8-1
+numbers= ['1', '2', '3', '4', '5']
+sum=0
+#Making a list of numbers for the loop and making a base variable that starts at zero
+for x in range(6):
+    sum+=x
+#The loop that adds each number in the list onto the last sum 
+print(sum)
---
+++ Lab_8-5.py
@@ -0,0 +1,11 @@
+#Lab_8-5
+
+word = "apple"
+count=0
+i = 0
+
+while i < len(word):
+    if word[i]== 'a':
+        count += 1
+    i += 1
+print (count)
